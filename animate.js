8d1a257fc19ef9b8c9a8f39e104ca4a7774892cc
Tim-Kim-1227 <ck2982@nyu.edu>
2022-11-23 04:52:35 +0400

    intital

---
+++ animate.js
@@ -0,0 +1,31 @@
+let changeWord;
+let count = 0;
+let index = 0;
+
+let head = document.getElementById('head');
+let word = document.getElementById('word');
+
+window.onload = function(){
+  word.style.visibility = "hidden";
+
+  document.addEventListener("mousemove", function(event) {
+  myFunction(event);
+  ChangeWord();
+});
+}
+
+function myFunction(e) {
+  word.style.left = e.pageX + 'px';
+  word.style.top = e.pageY + 'px';
+}
+
+function ChangeWord(){
+  word.style.visibility = "visible";
+  count++;
+  if(count > 70){
+    let sourceName = 'word' + index + '.png';
+    word.src = sourceName;
+    index = (++index) % 5;
+    count = 0;
+  }
+}
